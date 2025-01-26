45b4726ce9f52e9500246a956b697f47d8603c92
ProCrafter67 <162880510+ProCrafter67@users.noreply.github.com>
2025-01-26 16:01:44 +0530

    Small Changes

---
+++ DarkSidesOfTheMoon.cpp
@@ -0,0 +1,12 @@
+﻿// DarkSidesOfTheMoon.cpp : Defines the entry point for the application.
+//
+
+#include "DarkSidesOfTheMoon.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
